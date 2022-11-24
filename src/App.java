6f968ce84f5b9ed83534ccfa2eaab64abc5243a9
Matheus Freitas <matheusdefreitask9@hotmail.com>
2022-11-24 02:56:17 -0300

    alteracao visual

---
+++ src/App.java
@@ -0,0 +1,36 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sun.applet.Main;
+
+public class App extends Application {
+    public Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    private Parent root = null;
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            root = FXMLLoader.load(getClass().getResource("/view/listView.fxml"));
+        } catch (Exception e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "nao ta rodando", e);
+        }
+        setStage(primaryStage);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/controller/listViewController.java
@@ -0,0 +1,51 @@
+package controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class listViewController implements Initializable{
+
+    @FXML
+    private ListView<String> myListView;
+    @FXML
+    private Button btAdicionar;
+    @FXML
+    private Button btRemover;
+    @FXML
+    private Button btEditar;
+    @FXML
+    private TextField textField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    //Create
+    @FXML
+    public void adicionar(ActionEvent event){
+        if (!textField.getText().isEmpty())
+        {
+            myListView.getItems().add(textField.getText());
+            myListView.refresh();
+            textField.clear();
+        }
+        else{System.out.println("pppp");}
+
+    }
+
+
+    //Delete
+    @FXML
+    public void remover(ActionEvent event){
+        int index = myListView.getSelectionModel().getSelectedIndex();
+        myListView.getItems().remove(index);
+        myListView.refresh();
+    }
+}
